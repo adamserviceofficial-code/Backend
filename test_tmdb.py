81a79dfc294a2227624604e41d112c2c4aac772c
adamserviceofficial-code <adamserviceofficial@gmail.com>
2026-09-15 20:47:33 +0530

    Update test_tmdb.py

--- test_tmdb.py
+++ test_tmdb.py
@@ -1,7 +1,7 @@
 import asyncio
 from themoviedb import aioTMDb
 
-tmdb = aioTMDb(key="44cf8f2bba6890333d4ffcb6fa1dd7f6", language="en-US", region="US")
+tmdb = aioTMDb(api_key="44cf8f2bba6890333d4ffcb6fa1dd7f6", language="en-US", region="US")
 
 async def main():
     movies_details = await tmdb.movie(24428).details() # The Avengers
